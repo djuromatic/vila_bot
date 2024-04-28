58aa9a3c72fafdc08aeace8dab4ed9ef3ff56781
djuromatic <djuromatic991@gmail.com>
2024-04-28 14:08:15 +0200

    brojevi racuna

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:20-alpine
+WORKDIR /app
+
+COPY package.json /app
+COPY index.js /app
+
+RUN npm install
+
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,49 @@
+const { Telegraf } = require('telegraf')
+const { Markup } = require('telegraf')
+
+
+const bot = new Telegraf("6435064171:AAF53yRhPfsabCvdzm1HSPWZaDnSzJTm9tM")
+console.log('Bot has been started ...')
+// Define the menu
+const menu = Markup.inlineKeyboard([
+  Markup.button.callback('💬 Brojevi Racuna', 'racuni'),
+]);
+
+// Start command handler
+bot.start((ctx) => {
+  ctx.reply('Welcome to our bot! Here are some options:', menu);
+});
+
+const racuni = {
+  djuro: "115038163121423333",
+  pupovac: "160580010006985194",
+  srdjan: "325930050043846748",
+  strahinja: "265000000602894583",
+  filip: "160580010126897176",
+  bojan: "340000003242228810",
+  cici: "265201031000954538",
+  dean: "265000000656434024"
+}
+
+for (const [key, value] of Object.entries(racuni)) {
+  bot.action(key, (ctx) => {
+    ctx.reply(value);
+  });
+}
+
+const neki = Markup.inlineKeyboard(
+  Object.keys(racuni).map((name) => Markup.button.callback(name, name))
+);
+
+// Handle menu callbacks
+bot.action('racuni', (ctx) => {
+  ctx.reply('Izaberi osobu', neki);
+
+});
+
+bot.action('about', (ctx) => {
+  ctx.reply('You clicked "About"');
+});
+
+// Start the bot
+bot.launch();
